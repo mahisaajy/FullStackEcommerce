2314b8444b705ffa9ff7d3aaa3123e8a383597ab
Mahisa Ajy Kusuma <mahisaajy@gmail.com>
2025-08-22 00:16:01 +0700

    Products Router and Controllers.

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,19 +1,13 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { listProducts, getProductById, createProduct, updateProduct, deleteProduct } from './productsController';
 
 const router = Router();
 
 // products endpoints
-router.get('/', (req, res) => {
-    res.send('the list of producsts');
-});
-
-router.get('/:id', (req, res) => {
-    console.log(req.params);
-    res.send('A Products');
-})
-
-router.post('/', (req, res) => {
-    res.send('New product created');
-});
+router.get('/', listProducts);
+router.get('/:id', getProductById)
+router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 export default router;
